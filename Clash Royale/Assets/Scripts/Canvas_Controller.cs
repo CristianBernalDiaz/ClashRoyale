ee8b752fccaee9c19bf611e8bc5ec8305dada764
CristianBernalDiaz <144072011+CristianBernalDiaz@users.noreply.github.com>
2023-11-02 23:07:22 -0600

    Menudeslizable

---
+++ Clash Royale/Assets/Scripts/Canvas_Controller.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canvas_Controller : MonoBehaviour
+{
+    float posFinal;
+    bool oppenMenu;
+    public float time = 0.5f;
+
+    private void Start()
+    {
+        posFinal = Screen.width / 2;
+        
+    }
+}
